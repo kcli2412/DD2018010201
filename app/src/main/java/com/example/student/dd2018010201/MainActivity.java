d5563973edf9c96aa6973f6e482d214a894a97ae
kcli2412 <kcli2412@gmail.com>
2018-01-02 11:38:54 +0800

    add button1,button2,button3

--- app/src/main/java/com/example/student/dd2018010201/MainActivity.java
+++ app/src/main/java/com/example/student/dd2018010201/MainActivity.java
@@ -2,12 +2,41 @@ package com.example.student.dd2018010201;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button bt3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bt3 = (Button) findViewById(R.id.button3);
+        MyListener listener = new MyListener();
+        bt3.setOnClickListener(listener);
+    }
+
+    public void click1(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.button:
+                Toast.makeText(MainActivity.this, "Test1", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button2:
+                Toast.makeText(MainActivity.this, "Test2", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    class MyListener implements View.OnClickListener
+    {
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(MainActivity.this, "Test3", Toast.LENGTH_SHORT).show();
+        }
     }
 }
